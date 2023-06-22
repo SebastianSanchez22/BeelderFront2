82b65cf7e4b1a8cc3fbd167777393591438eda2d
SebastianSanchez22 <sebastiansaor22@gmail.com>
2023-06-22 02:53:53 -0500

    Fixing machines form typo

--- components/formMaquina.tsx
+++ components/formMaquina.tsx
@@ -25,78 +25,84 @@ export default function FormMaquina() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="max-w-md mx-auto">
-      <div className="mb-4">
-        <label htmlFor="machineId" className="block text-sm font-medium text-gray-700">
-          ID Máquina
-        </label>
-        <input
-          type="text"
-          id="machineId"
-          name="machineId"
-          value={formData.machineId}
-          onChange={handleChange}
-          className="mt-1 focus:ring-amber-500 focus:border-amber-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="name" className="block text-sm font-medium text-gray-700">
-          Nombre
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          className="mt-1 focus:ring-amber-500 focus:border-amber-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="category" className="block text-sm font-medium text-gray-700">
-          Categoría
-        </label>
-        <input
-          type="text"
-          id="category"
-          name="category"
-          value={formData.category}
-          onChange={handleChange}
-          className="mt-1 focus:ring-amber-500 focus:border-amber-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="totalQuantity" className="block text-sm font-medium text-gray-700">
-          Cantidad Total
-        </label>
-        <input
-          type="number"
-          id="totalQuantity"
-          name="totalQuantity"
-          value={formData.totalQuantity}
-          onChange={handleChange}
-          className="mt-1 focus:ring-amber-500 focus:border-amber-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="supplierId" className="block text-sm font-medium text-gray-700">
-          ID Proveedor
-        </label>
-        <input
-          type="text"
-          id="supplierId"
-          name="supplierId"
-          value={formData.supplierId}
-          onChange={handleChange}
-          className="mt-1 focus:ring-amber-500 focus:border-amber-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-        />
-      </div>
-      <button
-        type="submit"
-        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500"
-      >
-        Crear Máquina
-      </button>
-    </form>
+    <div className="flex justify-center items-center h-screen">
+      <form  onSubmit={handleSubmit} className="max-w-md mx-auto bg-black text-white p-8 rounded-lg shadow-lg">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div>
+            <label htmlFor="machineId" className="text-sm font-medium">
+              ID Máquina
+            </label>
+            <input
+              type="text"
+              id="machineId"
+              name="machineId"
+              value={formData.machineId}
+              onChange={handleChange}
+              className="mt-1 focus:ring-amber-500 focus:border-amber-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-amber-300 text-white"
+            />
+          </div>
+          <div>
+            <label htmlFor="name" className="text-sm font-medium">
+              Nombre
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="mt-1 focus:ring-amber-500 focus:border-amber-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-amber-300 text-white"
+            />
+          </div>
+          <div>
+            <label htmlFor="category" className="text-sm font-medium">
+              Categoría
+            </label>
+            <input
+              type="text"
+              id="category"
+              name="category"
+              value={formData.category}
+              onChange={handleChange}
+              className="mt-1 focus:ring-amber-500 focus:border-amber-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-amber-300 text-white"
+            />
+          </div>
+          <div>
+            <label htmlFor="totalQuantity" className="text-sm font-medium">
+              Cantidad Total
+            </label>
+            <input
+              type="number"
+              id="totalQuantity"
+              name="totalQuantity"
+              value={formData.totalQuantity}
+              onChange={handleChange}
+              className="mt-1 focus:ring-amber-500 focus:border-amber-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-amber-300 text-white"
+            />
+          </div>
+          <div>
+            <label htmlFor="supplierId" className="text-sm font-medium">
+              ID Proveedor
+            </label>
+            <input
+              type="text"
+              id="supplierId"
+              name="supplierId"
+              value={formData.supplierId}
+              onChange={handleChange}
+              className="mt-1 focus:ring-amber-500 focus:border-amber-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-amber-300 text-white"
+            />
+          </div>
+        </div>
+        <div className="flex justify-center mt-6">
+          <button
+            type="submit"
+            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-amber-500 hover:bg-amber-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500"
+          >
+            Crear Máquina
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
