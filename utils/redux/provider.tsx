e913def69a9bba3f4d4b602a531e9cd7ddabf4dc
SebastianSanchez22 <sebastiansaor22@gmail.com>
2023-06-29 11:13:46 -0500

    Remove providerApi, works with Redux provider only

--- utils/redux/provider.tsx
+++ utils/redux/provider.tsx
@@ -1,18 +1,12 @@
-import { Provider as ReduxProvider } from 'react-redux';
-import { ApiProvider } from '@reduxjs/toolkit/query/react';
-import { machinesApi } from './api/machinesApi';
-import { suppliersApi } from './api/suppliersApi';
+import { Provider } from 'react-redux';
 import { store } from './store';
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ReduxProvider store={store}> {/* Redux store */}
-      <ApiProvider api={machinesApi}>  {/* RTK Query machines API  */}
-        <ApiProvider api={suppliersApi}> {/* RTK Query suppliers API  */}
-          {children}
-        </ApiProvider>
-      </ApiProvider>
-    </ReduxProvider>
+    <Provider store={store}>
+      {children}
+    </Provider>
   );
 }
